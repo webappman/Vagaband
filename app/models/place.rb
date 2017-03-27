a0eeb7b9bcba68a62aa9e9bde8fcc7fcd2499a53
webappman <web730.man@outlook.com>
2017-03-27 15:40:34 +0200

    Adding Support for Coordinates

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,14 @@
 class Place < ApplicationRecord
-  geocoded_by :address
-  after_validation :geocode, if: ->(obj){ obj.address.present? and obj.address_changed?}
+  attr_accessor :raw_address
+
+  geocoded_by :raw_address
+  reverse_geocoded_by :latitude, :longitude
+
+  after_validation -> {
+    self.address = self.raw_address
+    geocode
+  }, if: ->(obj){ obj.raw_address.present? and obj.raw_address != obj.address }
+
+  after_validation :reverse_geocode, unless: ->(obj) { obj.raw_address.present? },
+                   if: ->(obj){ obj.latitude.present? and obj.latitude_changed? and obj.longitude.present? and obj.longitude_changed? }
 end
