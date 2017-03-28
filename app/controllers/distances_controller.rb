fcf9559d5910380443da3e2f0e954d706ef9af56
webappman <web730.man@outlook.com>
2017-03-28 08:50:14 +0200

    Complete!

--- app/controllers/distances_controller.rb
+++ app/controllers/distances_controller.rb
@@ -4,5 +4,12 @@ class DistancesController < ApplicationController
   end
 
   def create
+    @from = Place.find_by(id: params[:from])
+    @to = Place.find_by(id: params[:to])
+    if @from && @to
+      flash[:success] =
+          "The distance between <b>#{@from.title}</b> and <b>#{@to.title}</b> is #{@from.distance_from(@to.to_coordinates)} km"
+    end
+    redirect_to new_distance_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'distances/new'
 
-  get 'distances/create'
-
+  
   resources :places, except: [:update, :edit, :destroy]
+  resources :distances, only: [:new, :create]
   root 'places#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
